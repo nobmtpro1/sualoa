933eee9e8dbb36e236fa61abf22015d2390af66f
KienJerry <kienntpk01531@gmail.com>
2023-05-23 11:48:22 +0700

    push

--- wp-content/themes/custom/footer.php
+++ wp-content/themes/custom/footer.php
@@ -1,3 +1,72 @@
+<footer class="footer">
+    <div class="footer-contact container">
+        <div class="bg-layout-contact">
+            <div class="header-contact">
+                <div class="line"></div>
+                <h2>LIÊN HỆ NGAY CHO CHÚNG TÔI</h2>
+                <div class="line"></div>
+            </div>
+            <div class="form-contact">
+                <p>Thứ 2 - Chủ Nhật: 8AM - 7PM</p>
+                <p>☎️ HOTLINE:  <a href="tel:0767165660">0767 165 660</a></p>
+                <p>ĐỊA CHỈ: 249/44/3A Vườn Lài, P. Phú Thọ Hoà, Q. Tân Phú</p>
+            </div>
+            <div class="btn-call">
+                <i class="fa-solid fa-phone"></i>
+                GỌI THỢ NGAY
+            </div>
+        </div>
+    </div>
+
+    <div class="layout-footer container">
+        <div class="box-footer">
+            <div class="wrapper-box">
+                <h2>GIỚI THIỆU</h2>
+                <div class="line"></div>
+                <p>Trung tâm sửa điện lạnh - điện tử Suadienlanh là đơn vị chuyên sửa chữa các thiết bị điện lạnh, điện tử Uy tín, Giá rẻ và Chuyên nghiệp hàng đầu tại TPHCM, Hà Nội và Khắp cả nước.</p>
+                <p>Mục tiêu của chúng tôi là đem đến một dịch vụ tiện ích toàn diện cho khách hàng. Sự hài lòng của khách hàng chính là động lực giúp Suadienlanh.vn ngày càng phát triển.</p>
+                <strong>Địa Chỉ: 249/44/3a Vườn Lài, P. Phú Thọ Hoà, Q. Tân Phú</strong>
+                <div class="btn-hotline">Hotline 0767165660</div>
+            </div>
+            <div class="wrapper-box">
+                <h2>HỆ THỐNG CTV</h2>
+                <div class="line"></div>
+                <p>Chúng tôi hỗ trợ dịch vụ sửa điện lạnh - điện tử trên khắp địa bàn thành phố Hồ Chí Minh 24 quận - huyện, thành phố Thủ Đức.</p>
+                <div class="btn-ctv">HỆ THỐNG CTV TẠI TPHCM ⬇️</div>
+                <div class="bg-access-social">
+                    <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-goole.webp" alt="social">
+                    <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-mca.webp" alt="social">
+                </div>
+                <div class="box-bg-social">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-tiktok"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                </div>
+            </div>
+            <div class="wrapper-box">
+                <h2>DỊCH VỤ NỔI BẬT</h2>
+                <div class="line"></div>
+                <span>CÁC DỊCH VỤ ĐIỆN LẠNH</span>
+                <div class="bg-line"></div>
+                <span>CÁC DỊCH VỤ ĐIỆN TỬ</span>
+                <div class="bg-line"></div>
+                <span>ĐẶT LỊCH SỬA CHỮA</span>
+                <div class="bg-line"></div>
+                <span>TIN TỨC ĐIỆN LẠNH</span>
+                <div class="bg-line"></div>
+                <span>TIN TỨC ĐIỆN TỬ</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="box-Copyright container">
+        <p>Copyright 2023 © <span>Bản quyền thuộc về Suadienlanh.vn</span></p>
+    </div>
+
+
+</footer>
+
 <?php wp_footer() ?>
 <script src="<?= bloginfo('template_directory') ?>/assets/js/main.js"></script>
 </body>
--- wp-content/themes/custom/index.php
+++ wp-content/themes/custom/index.php
@@ -16,8 +16,169 @@
     </div>
 
     <!-- Kien -->
+    <div class="section-content">
+        <div class="wrapper-box-child">
+            <img src="<?= bloginfo('template_directory') ?>/assets/img/ic-clock.webp" alt="icon clock">
+            <p class="box-title">Thời gian làm việc</p>
+            <span class="box-desc">Thứ Hai – Chủ Nhật: 8AM – 7PM</span>
+        </div>
+        <div class="wrapper-box-child">
+        <img src="<?= bloginfo('template_directory') ?>/assets/img/ic-moreAccount.webp" alt="icon more account">
+            <p class="box-title">Địa chỉ</p>
+            <span class="box-desc">249/44/3a Vườn Lài, P. Phú Thọ Hoà, Q. Tân Phú, Tp.HCM</span>
+        </div>
+        <div class="wrapper-box-child">
+        <img src="<?= bloginfo('template_directory') ?>/assets/img/ic-moreAccount.webp" alt="icon more account">
+            <p class="box-title">Thời gian làm việc</p>
+            <span class="box-desc">0767 165 660</span>
+        </div>
+    </div>
 
-    
+    <div class="section-content-choose-us">
+        <div class="header-choose-us container">
+            <div class="line"></div>
+            <img src="<?= bloginfo('template_directory') ?>/assets/img/ic-gift-box.png" alt="icon gift">
+            <h2>TẠI SAO NÊN CHỌN CHÚNG TÔI</h2>
+            <div class="line"></div>
+        </div>
+        <div class="container-choose-us container">
+            <div class="content-container right">
+                <div class="wrapper-box-txt-choose-us">
+                    <h4>Uy tín</h4>
+                    <span>Cam kết không có tình trạng báo giá ảo, kỹ thuật viên luôn đến đúng giờ hẹn</span>
+                </div>
+                <div class="wrapper-box-txt-choose-us">
+                    <h4>NHANH CHÓNG </h4>
+                    <span>Đến tận nhà sửa chữa trong vòng 30 phút từ khi tiếp nhận thông tin khách hàng</span>
+                </div>
+            </div>
+            <div class="circle-img">
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-circle-choose-us.webp" alt="bg-circle-choose-us">
+                <div class="btn-see-more">
+                    <img src="<?= bloginfo('template_directory') ?>/assets/img/ic-reverse-arrow.png" alt="button ic">
+                    XEM THÊM</div>
+            </div>
+            <div class="content-container">
+                <div class="wrapper-box-txt-choose-us">
+                    <h4>GIÁ RẺ</h4>
+                    <span>Chi phí luôn được tối ưu đến mức thấp nhất, hỗ trợ miễn phí kiểm tra thiết bị</span>
+                </div>
+                <div class="wrapper-box-txt-choose-us">
+                    <h4>TƯ VẤN 24/7</h4>
+                    <span>Suadienlanh.vn hỗ trợ tư vấn khách hàng 24/7 bất kể ngày lễ hay chủ nhật</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-content-service">
+        <div class="header-service container">
+            <div class="line"></div>
+            <i class="fa-solid fa-bars"></i>
+            <h2>DỊCH VỤ SỬA ĐIỆN LẠNH - ĐIỆN TỬ NỔI BẬT</h2>
+            <div class="line"></div>
+        </div>
+        <div class="container-service container">
+            <div class="menu-navigation">
+                <div class="title-menu bottom">
+                    Sửa máy lạnh
+                </div>
+                <div class="title-menu bottom">
+                    Sửa máy giặt
+                </div>
+                <div class="title-menu bottom">
+                    Sửa lò vi sóng
+                </div>
+                <div class="title-menu bottom">
+                    Sửa máy nước nóng lạnh
+                </div>
+                <div class="title-menu bottom">
+                    Sửa cây nước nóng lạnh
+                </div>
+            </div>
+            <div class="menu-desc">
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-service.webp" alt="img bg">
+                <p>DỊCH VỤ SỬA MÁY LẠNH</p>
+                <span>Sở hữu đội ngũ kỹ thuật viên dày dặn kinh nghiệm, suadienlanh.vn tự tin cung cấp cho khách hàng dịch vụ sửa chữa máy lạnh Uy tín – Chất lượng hàng đầu tại TPHCM.</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-content-our-project">
+        <div class="header-content-our-project container">
+            <div class="line"></div>
+            <i class="fa-solid fa-check"></i>
+            <h2>DỰ ÁN CỦA CHÚNG TÔI</h2>
+            <div class="line"></div>
+        </div>
+        <div class="bg-img-our-project container">
+            <div class="grid-item item1" >
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-img1.jpg" alt="bg img">
+            </div>
+            <div class="grid-item item2" >
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-img2.jpg" alt="bg img">
+            </div>
+            <div class="grid-item item3" >
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-img3.jpg" alt="bg img">
+            </div>
+            <div class="grid-item item4" >
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-img4.jpg" alt="bg img">
+            </div>
+            <div class="grid-item item5" >
+                <img src="<?= bloginfo('template_directory') ?>/assets/img/bg-img5.jpg" alt="bg img">
+            </div>
+        </div>
+    </div>
+
+    <div class="section-content-achieved">
+        <div class="header-content-achieved container">
+            <i class="fa-solid fa-plus"></i>
+            <h2>NHỮNG GÌ CHÚNG TÔI ĐẠT ĐƯỢC</h2>
+        </div>
+        <div class="row-item-achieved container">
+            <div class="child-item-achieved">
+                <div class="title-achieved">KHÁCH HÀNG</div>
+                <span>6000+</span>
+            </div>
+            <div class="child-item-achieved top">
+                <div class="title-achieved">ĐỐI TÁC</div>
+                <span>100+</span>
+            </div>
+            <div class="child-item-achieved">
+                <div class="title-achieved">NĂM KINH NGHIỆM</div>
+                <span>5+</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-content-blog">
+        <div class="header-content-blog container">
+            <div class="line"></div>
+            <h2>TIN TỨC GẦN ĐÂY</h2>
+            <div class="line"></div>
+        </div>
+        <div class="container-blog">
+            <div class="box-blog-item">
+                <h5>Máy lạnh Toshiba báo lỗi 14 – Nguyên nhân và cách khắc phục</h5>
+                <p>22/05/2023</p>
+                <div class="line"></div>
+                <span>Dù có tối ưu đến đâu thì qua thời gian sử dụng, máy lạnh Toshiba [...]</span>
+            </div>
+            <div class="box-blog-item">
+                <h5>Máy lạnh Toshiba báo lỗi 14 – Nguyên nhân và cách khắc phục</h5>
+                <p>22/05/2023</p>
+                <div class="line"></div>
+                <span>Dù có tối ưu đến đâu thì qua thời gian sử dụng, máy lạnh Toshiba [...]</span>
+            </div>
+            <div class="box-blog-item">
+                <h5>Máy lạnh Toshiba báo lỗi 14 – Nguyên nhân và cách khắc phục</h5>
+                <p>22/05/2023</p>
+                <div class="line"></div>
+                <span>Dù có tối ưu đến đâu thì qua thời gian sử dụng, máy lạnh Toshiba [...]</span>
+            </div>
+        </div>
+    </div>
+
     <!-- Kien -->
 </div>
 
